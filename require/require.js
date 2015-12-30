12fb2bfd1e580c577a372f5013b2b0c751dfefc0
thatsme <shailesh.nighojkar@gslab.com>
2015-12-30 19:42:05 +0530

    30-12-2015

---
+++ require/require.js
@@ -0,0 +1,21 @@
+function require(filename){
+
+	//new ObjectOrientedJavascriptSyntax().initialize();
+
+	function ObjectOrientedJavascriptSyntax(){
+
+		initialize: function(){
+			console.log("initialize");
+		}
+		// var xhttp = new XMLHttpRequest();
+  // 		xhttp.onreadystatechange = function() {
+  //   		if (xhttp.readyState == 4 && xhttp.status == 200) {
+  //     			console.log(xhttp.responseText);
+  //   		}
+  // 		};
+  // 		xhttp.open("GET", filename, true);
+  // 		xhttp.send();	
+	}
+
+	
+}
